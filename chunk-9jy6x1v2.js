79916483468fc78ece9d961a8cd8c992c62cfb08
rio01100801-gif <rio01100801-gif@users.noreply.github.com>
2026-08-15 04:29:19 +0900

    deploy: forge-v88

---
+++ chunk-9jy6x1v2.js
@@ -0,0 +1,2 @@
+import{lc as s}from"./chunk-75b392mx.js";import{Dc as o}from"./chunk-cchwjcan.js";var t=o(s(),1);function c({href:e,children:a,onClick:n,...i}){let r=typeof globalThis<"u"&&globalThis.__HASH_NAV__;return t.createElement("a",{href:r?`#${e}`:e,onClick:n,...i},a)}
+export{c as Oa};
